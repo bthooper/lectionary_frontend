0dadef6dec4294050012f9cc73bb084e8a2507e0
Bryan Hooper <bthooper@gmail.com>
2020-12-10 13:11:19 -0500

    added semantic ui CSS and also continued work on form in SelectLectionary component

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import SelectLectionary from "./SelectLectionary";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SelectLectionary />
       </div>
     );
--- src/components/SelectLectionary.js
+++ src/components/SelectLectionary.js
@@ -2,7 +2,20 @@ import React from "react";
 
 class SelectLectionary extends React.Component {
   render() {
-    return <div>Select Lectionary </div>;
+    return (
+      <div className="select-lectionary ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Select Lectionary</label>
+            <select name="lectionary" id="lectionary">
+              <option value="rcl">Revised Common Lectionary</option>
+              <option value="nl">Narrative Lectionary</option>
+              <option value="ol">Open Lectionary</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
