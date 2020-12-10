75f655a38608a51405901cdaeba16e5310ba1a98
Bryan Hooper <bthooper@gmail.com>
2020-12-10 12:57:16 -0500

    initial set up of first component: SelectLectionary

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import SelectLectionary from "./SelectLectionary";
 
 class App extends React.Component {
   render() {
-    return <div>Lectionary App</div>;
+    return (
+      <div>
+        <SelectLectionary />
+      </div>
+    );
   }
 }
 
---
+++ src/components/SelectLectionary.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SelectLectionary extends React.Component {
+  render() {
+    return <div>Select Lectionary </div>;
+  }
+}
+
+export default SelectLectionary;
